a792f8e510979b4f615d2b9615d4887200debcb0
Bruno Azoulay <info@fusengine.ch>
2021-10-18 21:46:14 +0200

    features(sanctum): add authentification login rergister and logout

---
+++ app/Http/Controllers/Api/Auth/AuthenticationController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers\Api\Auth;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\Auth\LoginRequest;
+use App\Http\Requests\Auth\RegisterRequest;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+
+class AuthenticationController extends Controller
+{
+    /**
+     * Permet de créer un compte utilisateur
+     *
+     * @param RegisterRequest $request
+     * @return mixed
+     */
+    public function register(RegisterRequest $request)
+    {
+        $user = User::create([
+            'name'     => $request->name,
+            'email'    => $request->email,
+            'password' => Hash::make($request->password),
+        ]);
+
+        //$request->session()->regenerate();
+        $createToken = $user->createToken("Token Register $user->name")->plainTextToken;
+
+        return response()->json([
+            'token'         => $createToken,
+            'Authorization' => 'Bearer',
+        ], 200);
+    }
+
+    /**
+     * Permet a l'utilisateur de ce connecté
+     *
+     * @param LoginRequest $request
+     * @return mixed
+     */
+    public function login(LoginRequest $request)
+    {
+        if (!Auth::attempt($request->only('email', 'password'))) {
+
+            $request->session()->regenerate();
+
+            return response()->json([
+                'errors' => [
+                    'email'    => ['Email is invalid'],
+                    'password' => ['Password is wrong'],
+                ],
+            ], 422);
+        }
+
+        $token = auth()
+            ->user()
+            ->createToken("Token Generate by " . Auth::user()->name)
+            ->plainTextToken;
+
+        return response()->json([
+            'token_access' => $token,
+            'token_type'   => "Bearer {$token}",
+        ], 200);
+
+    }
+
+    /**
+     * Permet de ce déconnecté
+     *
+     * @return mixed
+     */
+    public function logout(Request $request)
+    {
+        Auth::guard('web')->logout();
+
+        $request->user()->currentAccessToken()->delete();
+
+        return response()->json([
+            'message' => 'You are logout'], 200);
+
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Api\Auth\AuthenticationController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -12,8 +13,14 @@ use Illuminate\Support\Facades\Route;
 | routes are loaded by the RouteServiceProvider within a group which
 | is assigned the "api" middleware group. Enjoy building your API!
 |
-*/
+ */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::post('/register', [AuthenticationController::class, 'register']);
+Route::post('/login', [AuthenticationController::class, 'login']);
+
+Route::group(['middleware' => ['auth:sanctum']], function () {
+    Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+        return $request->user();
+    });
+    Route::post('/logout', [AuthenticationController::class, 'logout']);
 });
